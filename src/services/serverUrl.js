de0dca4f5db768a586749d69779210a27b53a7b4
Aneesh-o <aneeshkadampazhipuram.com>
2025-06-18 13:50:59 +0530

    message

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,4 +1,4 @@
-// const serverUrl = "https://careerconnect-server-owt8.onrender.com"
-const serverUrl = "http://localhost:3000"
+const serverUrl = "https://careerconnect-server-owt8.onrender.com"
+// const serverUrl = "http://localhost:3000"
 
 export default serverUrl
